1d913f94baa4ed41b83883c575085b3ca2cabf41
Fabio <binhobinho1988@hotmail.com>
2022-12-04 18:48:53 -0300

    full-commit

--- p11/assets/js/script.js
+++ p11/assets/js/script.js
@@ -1,6 +1,21 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
 
+function calcular(){
 
+    const qtdePessoas = document.querySelector("#qtdePessoas").value;  
+    const consumoMedio = document.querySelector("#consumoMedio").value;
+    const valorCarne = document.querySelector("#valorCarne").value;
+    const totalCarneForm = document.querySelector("#totalCarne");
+    const totalPorPessoaForm = document.querySelector("#totalPorPessoa");  
+    
+    if(qtdePessoas > 0){
+
+        let totalCarne = consumoMedio * qtdePessoas;
+        let precoTotal = totalCarne * valorCarne;
+        let totalPorPessoa = precoTotal / qtdePessoas;  
+        totalCarneForm.innerHTML = totalCarne;
+        totalPorPessoaForm.innerHTML = totalPorPessoa;
+
+    } else {
+        document.querySelector("#error").innerHTML = "A quantidade de pessoas não pode ser inferior à 1. Tente novamente!";
+    }
+}
