c9786d1b3431e27d6b9f2ff650e251064a5786e2
Fabio <binhobinho1988@hotmail.com>
2022-12-04 18:49:44 -0300

    full-commit

--- p11/assets/js/script.js
+++ p11/assets/js/script.js
@@ -8,14 +8,13 @@ function calcular(){
     const totalPorPessoaForm = document.querySelector("#totalPorPessoa");  
     
     if(qtdePessoas > 0){
-
         let totalCarne = consumoMedio * qtdePessoas;
         let precoTotal = totalCarne * valorCarne;
         let totalPorPessoa = precoTotal / qtdePessoas;  
         totalCarneForm.innerHTML = totalCarne;
         totalPorPessoaForm.innerHTML = totalPorPessoa;
-
     } else {
         document.querySelector("#error").innerHTML = "A quantidade de pessoas não pode ser inferior à 1. Tente novamente!";
     }
+    
 }
